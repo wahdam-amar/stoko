2d6f074cddce81ff4ba2f1c787eedeca5a1a7a99
wahdam <wahdam.amarulloh@gmail.com>
2021-02-24 23:29:38 +0700

    add basic css and js

---
+++ database/migrations/2021_02_24_155647_create_companies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->integer('company_id')->unsigned()->unique();
+            $table->text('name');
+            $table->text('description');
+            $table->timestamps();
+
+            $table->index('company_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
